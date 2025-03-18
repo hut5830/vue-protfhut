a8d2a80d4ad3f57230b8ebc16ea51231d885a51b
tatchakorn.c@jmtnetwotk.co.th <watchrapon.w>
2025-03-19 02:12:04 +0700

    UpdateNewProtfHut

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1 class="text-green-600">Hello I'm Tatchakorn</h1>
+        <p class="back">Hi, I'm [Your Name], a [Your Profession].</p>
+        <a-input placeholder="Basic usage" />
+        <a-button type="primary">Primary Button</a-button>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
