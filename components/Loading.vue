eacc3da18b205f723fc6bc5026c1d2cab9bfd4dc
tatchakorn.c@jmtnetwotk.co.th <watchrapon.w>
2025-03-21 14:22:08 +0700

    UpdateUIProfile

---
+++ components/Loading.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+    <a-modal :open="isLoading" :closable="false" :maskClosable="false" :centered="true" :footer="null">
+        <div class="loading-spinner">
+            <a-spin size="large" />
+            <p>Loading...</p>
+        </div>
+    </a-modal>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+    isLoading: {
+        type: Boolean,
+        required: true
+    }
+});
+</script>
+
+<style scoped>
+
+</style>
