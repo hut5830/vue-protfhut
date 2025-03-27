43f0912bf309887183b7de1b0fdff47bce7c2884
tatchakorn.c@jmtnetwotk.co.th <watchrapon.w>
2025-03-27 22:59:24 +0700

    UpdateNoti

---
+++ components/NotiBasic.vue
@@ -0,0 +1,26 @@
+<template>
+    <contextHolder />
+</template>
+<script lang="ts" setup>
+import {
+    RadiusBottomleftOutlined,
+    RadiusBottomrightOutlined,
+    RadiusUpleftOutlined,
+    RadiusUprightOutlined,
+} from '@ant-design/icons-vue';
+import type { NotificationPlacement } from 'ant-design-vue';
+import { notification } from 'ant-design-vue';
+const [api, contextHolder] = notification.useNotification();
+const open = (placement: NotificationPlacement) => openNotification(placement);
+const openNotification = (placement: NotificationPlacement) => {
+    api.success({
+        message: `เชื่อมต่อโปรไฟล์สำเร็จ (No Authen)`,
+        description:
+            'สามารถดูตัวอย่างโปรเจคกับรายละเอียดเบื้องต้นได้ที่ปุ่ม => (ดูรายละเอียด)',
+        placement,
+    });
+};
+onMounted(() => {
+    openNotification('topRight');
+});
+</script>
